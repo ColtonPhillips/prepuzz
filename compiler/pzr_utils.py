668743cdd0d00e5ee5f130f20d8909bd01359a81
Colton Phillips <colton@sodium.hitronhub.home>
2017-10-21 02:07:41 -0700

    created a file and refactored a bit of the compiler

--- compiler/pzr_utils.py
+++ compiler/pzr_utils.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 
+import re
 import os
+from PIL import Image
+import sys
+
+voxel_dir = "../voxels/"
+src_dir = "../src/"
+image_dir = "../images/"
+out_dir = "../out/"
+voxel_dir = "../voxels/"
+text_dir = "../texts/"
+rules_dir = "../rules/"
+levels_dir = "../levels/"
+objects_dir = "../objects/"
+game_file = "dungeon.predude"
+legend_file = "../texts/legend.txt"
 
 def file_to_lines(file_path):
   with open(file_path) as f:
@@ -12,3 +27,18 @@ def files_in_folder(extension, folder):
     if f.endswith(extension):
       files.append(os.path.join(folder, f))
   return files
+
+def addUnique(l,item):
+  if item not in l:
+    l.append(item)
+  return l
+
+def atoi(text):
+  return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+  return [ atoi(c) for c in re.split('(\d+)', text) ]
+
+def pixels_w_h(image_path):
+  im = Image.open(image_path).convert('RGBA')
+  return im.load(), im.size[0], im.size[1]
